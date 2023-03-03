e0461d42cf1242c5b7a5f6f53b251b6f0f52878f
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-03-02 22:18:52 -0300

    feat(Posts): Criação do corpo temporário da página de posts

---
+++ rocketseat-redesocial/src/Posts.jsx
@@ -0,0 +1,8 @@
+export function Posts(){
+    return(
+        <div>
+            <strong>Larissa Soares Reis</strong>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate ea numquam beatae fugit quaerat debitis, veritatis distinctio, mollitia porro minus deleniti sequi, deserunt dolores cum eum sunt adipisci perferendis modi.</p>
+        </div>
+    )
+}
