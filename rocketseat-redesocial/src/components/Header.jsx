420f1b5eb0bc5606678e8bc5852de3d0981a6754
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-03-02 22:38:40 -0300

    feat(CSS Module): Aplicação dos estilos no Header

--- rocketseat-redesocial/src/components/Header.jsx
+++ rocketseat-redesocial/src/components/Header.jsx
@@ -1,6 +1,8 @@
+import styles from './Header.module.css'
+
 export function Header(){
     return(
-        <div>
+        <div  className={styles.header}>
             <strong>Ignite Feed</strong>
         </div>
     )
