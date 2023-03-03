7887f02665612c3d190c30b364e1197bd9cdba17
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-03-02 22:41:01 -0300

    feat(App): Inserção dos Posts na página principal

--- rocketseat-redesocial/src/App.jsx
+++ rocketseat-redesocial/src/App.jsx
@@ -1,9 +1,10 @@
 import { Posts } from './Posts'
+import { Header } from './components/Header'
 
 export function App() {
   return (
     <div>
-
+      <Header />
       <Posts />
     </div>
   )
