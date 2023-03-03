0e4e200030abcbb0189f655f131f5515797c7c42
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-03-02 22:45:03 -0300

    feat(App): Adição do conteúdo do post inserido dinamicamente

--- rocketseat-redesocial/src/App.jsx
+++ rocketseat-redesocial/src/App.jsx
@@ -5,7 +5,10 @@ export function App() {
   return (
     <div>
       <Header />
-      <Posts />
+      <Posts 
+        name="Larissa Soares Reis"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos sunt officiis nulla laboriosam. Quae dolores, accusantium quaerat asperiores exercitationem nesciunt reprehenderit placeat ipsum itaque maxime enim ullam maiores recusandae assumenda?"
+      />
     </div>
   )
 }
