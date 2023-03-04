a8273b553c0988c83d4d3cc4413c02823b53750c
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-03-04 11:29:32 -0300

    feat(Header): Adição do logo da ignite no header

--- rocketseat-redesocial/src/components/Header.jsx
+++ rocketseat-redesocial/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import styles from './Header.module.css'
+import igniteLogo from '../assets/ignite-logo.svg'
 
 export function Header(){
     return(
         <div  className={styles.header}>
+            <img src={igniteLogo} alt="Logotipo da Ignite" />
             <strong>Ignite Feed</strong>
         </div>
     )
