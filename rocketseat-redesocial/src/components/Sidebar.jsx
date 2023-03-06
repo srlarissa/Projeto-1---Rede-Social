5e46d34d999c0b471646f12d6058cdae6d0fdabf
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-03-06 09:58:30 -0300

    feat(CSS): Adição de foto de capa

--- rocketseat-redesocial/src/components/Sidebar.jsx
+++ rocketseat-redesocial/src/components/Sidebar.jsx
@@ -3,7 +3,10 @@ import styles from './Sidebar.module.css';
 export function Sidebar(){
     return(
         <aside className={styles.sidebar}>
-            <img src="https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=50" />
+            <img 
+                className={styles.cover} 
+                src="https://images.unsplash.com/photo-1560114928-40f1f1eb26a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+            />
             <div className={styles.profile}>
                 <strong>Larissa Soares Reis</strong>
                 <span>Web Developer</span>
