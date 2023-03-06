b0b258e428ad21ca0d0d18f26fab386dbc7463f4
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-03-06 09:32:53 -0300

    feat(html): Adição e organizaçãod da estrutura dos posts e sidebar

--- rocketseat-redesocial/src/App.jsx
+++ rocketseat-redesocial/src/App.jsx
@@ -1,15 +1,23 @@
-import { Posts } from './Posts'
-import { Header } from './components/Header'
-import './global.css'
+import { Posts } from './Posts';
+import { Header } from './components/Header';
+import './global.css';
+import styles from '../src/components/App.module.css';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Posts 
-        name="Larissa Soares Reis"
-        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos sunt officiis nulla laboriosam. Quae dolores, accusantium quaerat asperiores exercitationem nesciunt reprehenderit placeat ipsum itaque maxime enim ullam maiores recusandae assumenda?"
-      />
+      <div className={styles.wrapper}>
+        <aside>
+          Sidebar
+        </aside>
+        <main>
+            <Posts 
+              name="Larissa Soares Reis"
+              content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis asperiores dicta alias totam facilis? Laboriosam eum sint cumque eaque maiores aperiam doloribus, voluptates a ab molestiae voluptate quo ipsum vitae."
+            />
+          </main>
+      </div>
     </div>
-  )
+  );
 }
