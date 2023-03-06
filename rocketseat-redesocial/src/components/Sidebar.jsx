7c6c8e66f5a6429d80d5cf2be3a1dee8c4e59330
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-03-06 09:47:38 -0300

    feat(App): Detalhamento da estrutura da sidebar

--- rocketseat-redesocial/src/components/Sidebar.jsx
+++ rocketseat-redesocial/src/components/Sidebar.jsx
@@ -2,8 +2,17 @@ import styles from './Sidebar.module.css';
 
 export function Sidebar(){
     return(
-        <aside>
-            Sidebar
+        <aside className={styles.sidebar}>
+            <img src="https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=50" />
+            <div className={styles.profile}>
+                <strong>Larissa Soares Reis</strong>
+                <span>Web Developer</span>
+            </div>
+            <footer>
+                <a href="#">
+                    Editar seu perfil
+                </a>
+            </footer>
         </aside>
     )
 }
