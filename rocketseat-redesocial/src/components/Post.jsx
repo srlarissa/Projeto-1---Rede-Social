7c2f59a83ad34771b54b0b862e355c63309c004b
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-03-07 19:16:33 -0300

    feat(Posts): Estilização dos posts

--- rocketseat-redesocial/src/components/Post.jsx
+++ rocketseat-redesocial/src/components/Post.jsx
@@ -1,3 +1,26 @@
+import styles from './Post.module.css';
+
 export function Post() {
+    return(
+        <article className={styles.post}>
+            <header>
+
+                <div className={styles.author}>
+                    <img 
+                        className={styles.avatar}
+                        src="https://github.com/srlarissa.png" 
+                    />
+                    <div className={styles.authorInfo}>
+                        <strong>Larissa Soares Reis</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+              <time dateTime='2023-03-07 16:00:00'>
+                Publicado há 1h
+              </time>
+            </header>
+        </article>
+    )
     
 }
