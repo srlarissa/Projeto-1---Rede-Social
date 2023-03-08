1255334446ce54571f94f28a6f59d57156875218
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-03-08 09:52:25 -0300

    feat(Comments): Criação da estrutura do componente

--- rocketseat-redesocial/src/components/Comments.jsx
+++ rocketseat-redesocial/src/components/Comments.jsx
@@ -1,5 +1,37 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comments.module.css';
 
 export function Comments() {
-
+    return(
+        <div className={styles.comment}>
+            <img 
+                src="https://github.com/srlarissa.png" 
+            />
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Larissa Soares Reis</strong>
+                            <time 
+                                title="07 de Março às 16h" 
+                                dateTime='2023-03-07 16:00:00'
+                            >
+                                Cerca de 1h atrás
+                            </time>
+                            <button title="Deletar comentário">
+                                <Trash />
+                            </button>
+                        </div>
+                    </header>
+                    <p>Muito bom Devon! Parabéns!</p>
+                </div>
+                <footer>
+                    <button title="Aplaudir">
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
 }
