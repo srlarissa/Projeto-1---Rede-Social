b75069eaec67858489d6c10cea587c1448f39121
Larissa Soares Reis <larissasoaresreis@gmail.com>
2023-03-15 10:47:10 -0300

    feat: config commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    extends: ['@commitlint/config-conventional'],
+    rules:{
+        'type-enum': [
+            2,
+            'always',
+            ['build', 'chore', 'ci', 'docs', 'improvement', 'feat', 'fix', 'perf', 'refactor', 'style', 'test'],
+        ]
+    }
+};
